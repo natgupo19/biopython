ed15d224ad6a65736d57bd011a356d546c6f8ab3
[natgupo19] <[natgupo@lcg.unam.mx]>
2022-08-30 17:42:04 -0500

    tarea 1

--- src/AT_rich.py
+++ src/AT_rich.py
@@ -12,7 +12,8 @@ GITHUB
     https://github.com/natgupo19/biopython
     
 DESCRIPTION
-     
+    Programa que identidica las regiones ricas en AT de 
+    un archivo con una secuencia de DNA
 
 CATEGORY
     DNA sequence
@@ -26,10 +27,6 @@ ARGUMENTS
                         File with DNA sequence
     -s SIZE, --size SIZE
                         Minimum size to search
-    -o path/to/output/file, --output path/to/output/file
-                        Path for the output file
-    -r ROUND, --round ROUND
-                        Number of digits to round
                         
 INPUT
     Secuencia de DNA a analizar
@@ -52,30 +49,43 @@ parser.add_argument("-s", "--size",
                     help="cantidad minima de AT a buscar",
                     type=int,
                     required=False)
-
-parser.add_argument("-o", "--output",
-                    metavar="path/to/output/file",
-                    help="Path for the output file",
-                    required=False)
-                    
-parser.add_argument("-r", "--round",
-                    help="Number of digits to round",
-                    type=int,
-                    required=False)
-                    
+                                        
 args = parser.parse_args()
 
 with open(args.file, 'r') as sequence:
     dna = sequence.read().upper()
 
 def validate(dna):
-    invalid = re.finditer("[^ATCG]+", dna)
-    match = len([invalid])
-    if match:
-        for error in invalid: 
-            print(f"Existen bases invalidas en el archivo introducido: ")
-        return(0)
-    else:
+    invalid = re.finditer("[^ATCG+]", dna)
+    match = len([*invalid]) 
+    if not match:
         return(1)
+    if match:
+        invalid = re.finditer("[^ATCG+]", dna)
+        for error in invalid:
+            print (f"\nSe encontaro un caracter invalido: {error.group()} en la posicion {error.span()}\n")
+        return(0)
+    
+def at_regions(dna, at = 13):
+    at_rich = re.finditer("([AT]+)", dna)
+    match_2 = len([*at_rich])
+    if match_2:
+        at_rich = re.finditer("([AT]+)", dna)
+        for regions in at_rich:
+            if len(regions.group()) >= at:
+                print(f"\nSe encontro la region rica en AT: {regions.group()} en la posicion {regions.span()}\n")
+    else:
+        print("\nNo se encontraron regiones ricas en AT\n")
+            
+if validate(dna):
+    if args.size:
+        result = at_regions(dna, args.size)
+    
+    else:
+        result = at_regions(dna)
+        
+
+    
         
     
+            
--- src/fg.py
+++
@@ -1,7 +0,0 @@
-import re
-
-invalid = re.finditer("[^ATCG]+", "TGFA")
-matches = len([invalid])
-
-print(i
-print(matches)
